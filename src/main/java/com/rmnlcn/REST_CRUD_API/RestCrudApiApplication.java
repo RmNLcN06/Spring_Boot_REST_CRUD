421bc058629684f57611ebc9c5f00e731f24bbfa
RmNLcN06 <romain.luciano@orange.fr>
2025-01-23 15:37:03 +0100

    Initial commit

---
+++ src/main/java/com/rmnlcn/REST_CRUD_API/RestCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.REST_CRUD_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudApiApplication.class, args);
+	}
+
+}
