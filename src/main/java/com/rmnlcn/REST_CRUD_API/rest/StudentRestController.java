2583611185317ab54a7cad3abde3aa59890632e0
RmNLcN06 <romain.luciano@orange.fr>
2025-01-25 18:32:21 +0100

    Update REST service to throw exception

--- src/main/java/com/rmnlcn/REST_CRUD_API/rest/StudentRestController.java
+++ src/main/java/com/rmnlcn/REST_CRUD_API/rest/StudentRestController.java
@@ -35,6 +35,10 @@ public class StudentRestController {
     // define endpoint for "/students/{studentId}" - return student at index
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        // check the studentId again list size
+        if((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudents.get(studentId);
     }
 }
