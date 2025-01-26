9623cf0a556cc21a22b67f954930758956b3715b
RmNLcN06 <romain.luciano@orange.fr>
2025-01-26 21:15:21 +0100

    Create new @ControllerAdvice for global exception handling

---
+++ src/main/java/com/rmnlcn/REST_CRUD_API/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.rmnlcn.REST_CRUD_API.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+    // add exception handling code
+
+}
